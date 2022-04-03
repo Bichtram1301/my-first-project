e3fb2a260159192a124f572ea59be5f894d4612d
Bichtram1301 <bichtram13197@gmail.com>
2022-04-03 23:33:08 +0700

    My First commit

---
+++ lesson2.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+
+public class lesson2 {
+	
+	public static void main(String[] args) {
+		
+		String name = JOptionPane.showInputDialog("Enter your name ");
+		JOptionPane.showMessageDialog(null, "Hello "+name);
+		
+		int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age "));
+		JOptionPane.showMessageDialog(null, "Your age is "+age+" years old");
+		
+		Double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height "));
+		JOptionPane.showMessageDialog(null, "Your height is "+height+" cm tall");
+			
+				
+	}
+
+}
